cc82b20a319e66b5c6937d8b00939fb70db253cc
hugo <hugo.bally@doctolib.com>
2021-11-11 16:59:50 +0100

    Oversized images and logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image'
 
-// Handle no image TODO
-const ListingItem = ({ listing }) => {
+const ListingItem = ({ listing, index }) => {
   const { release, localImage } = listing
 
   const image = getImage(localImage)
 
   if (!image) return null
 
+  const className = index % 5 === 0 ? 'row-span-2 col-span-2' : ''
+
   return (
-    <div>
+    <div className={className}>
       <GatsbyImage
         image={image}
         alt={`${release.artist} ${release.description}`}
@@ -20,16 +21,39 @@ const ListingItem = ({ listing }) => {
   )
 }
 
+// Import image via a component so the build fails if not found / Or use URL ? TODO
 const IndexPage = ({ data }) => {
   return (
-    <div className="grid ">
-      {data.allListing.nodes.map((listing) => (
-        <ListingItem key={listing.id} listing={listing} />
-      ))}
-    </div>
+    <>
+      <header>
+        <div className="flex place-content-center">
+          <StaticImage
+            src="../images/ajna-records-logo-500x500.jpg"
+            alt="Ajna Records"
+            width={200}
+            height={200}
+          />
+        </div>
+      </header>
+      <main
+        className="grid
+          grid-cols-auto-fit
+          grid-flow-row-dense
+          place-items-center
+          gap-10
+          max-w-7xl
+          mx-auto
+          px-10"
+      >
+        {data.allListing.nodes.map((listing, index) => (
+          <ListingItem key={listing.id} listing={listing} index={index} />
+        ))}
+      </main>
+    </>
   )
 }
 
+// Load big size only for oversized images
 export const query = graphql`
   {
     allListing {
@@ -42,7 +66,7 @@ export const query = graphql`
         localImage {
           childImageSharp {
             gatsbyImageData(
-              width: 200
+              width: 600
               placeholder: BLURRED
               formats: [AUTO, WEBP, AVIF]
             )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto-fit': 'repeat(auto-fit, minmax(200px, 1fr))'
+      }
+    },
   },
   variants: {
     extend: {},
